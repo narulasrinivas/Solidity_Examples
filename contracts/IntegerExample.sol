bd0c01c0f19fc994c6fb61c644a73627cb37e5c0
Srinivas Narula <srinivas.n27@gmail.com>
2021-04-14 17:06:39 +0100

    first commit

---
+++ contracts/IntegerExample.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.3;
+
+contract IntegerExample{
+    uint public myUint;
+
+    function setmyUint(uint _myUint) public {
+        myUint = _myUint;
+    }
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+const IntegerExample = artifacts.require("IntegerExample");
+
+module.exports = function(deployer) {
+  deployer.deploy(IntegerExample);
+};
